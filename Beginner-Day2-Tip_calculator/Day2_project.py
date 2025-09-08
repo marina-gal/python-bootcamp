5051b7424a27cd0ea2e1c68355527ff271c8cd49
Marina <marinarodrigogaliana@gmail.com>
2025-09-08 21:29:23 +0200

    Update Day2_project.py

--- Beginner-Day2-Tip_calculator/Day2_project.py
+++ Beginner-Day2-Tip_calculator/Day2_project.py
@@ -1,1 +1,9 @@
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("How much tip would you like to give? 10, 12, or 15?"))
+people = int(input("How many people to split the bill?"))
 
+operation = (((tip/100) * bill) + bill)/people
+round_price = round(operation,2)
+
+print(f"Each person should pay: ${round_price}")
