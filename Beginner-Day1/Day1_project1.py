bf7e4b1519b3768be1c08a01966fa2bcbed1d4f5
Marina <marinarodrigogaliana@gmail.com>
2025-09-08 12:59:23 +0200

    Delete Beginner-Day1/Day1_project1.py

--- Beginner-Day1/Day1_project1.py
+++
@@ -1,5 +0,0 @@
-print("Welcome to the Band Name Generator. Find the name of your band.")
-city_grew_up = input("What's the name of the city you grew up in?\n")
-pets_name = input("What's your pet's name?\n")
-
-print("Your band name could be" + city_grew_up + " " +pets_name)
