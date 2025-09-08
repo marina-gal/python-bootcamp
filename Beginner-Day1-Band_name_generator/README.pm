06f272430c6cd94e38907cf0d03291431bdb24dd
Marina <marinarodrigogaliana@gmail.com>
2025-09-08 13:02:37 +0200

    Create README.pm

---
+++ Beginner-Day1-Band_name_generator/README.pm
@@ -0,0 +1,14 @@
+## Day 1 Project - Band Name Generator
+
+The program generates a fun band name by combining the name of your city and your pet's name.
+
+Applied concepts:
+
+    print()
+    input()
+    len()
+    variables
+    string concatenation
+    special characters as '\n'
+
+    
