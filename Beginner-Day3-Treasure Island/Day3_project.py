4045780717a110f4b796be18b4a1d4100c592dc2
Marina Rodrigo <marinarodrigogaliana@gmail.com>
2025-09-19 18:48:52 +0200

    Update Day3_project.py

--- Beginner-Day3-Treasure Island/Day3_project.py
+++ Beginner-Day3-Treasure Island/Day3_project.py
@@ -1,1 +1,51 @@
+print(r'''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\ ` . "-._ /_______________|_______
+|                   | |o ;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+direction1 = input('You\'re at a crossroad and you find two possible paths. '
+                   'Where do you want to go?\n Type "left" or "right" \n').lower()
+# quick tip, using the  \ before apostrophe in order to be able to use it inside de ''
+# using the lower() / or just saying if direction1 == "right" or direction1 == "LEFT"
 
+
+if direction1 == "right":
+    print("Sorry! You've fallen into a hole filled of sharped knives. Game over!")
+
+elif direction1 == "left":
+    choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake.\n Type "wait" to wait for a boat. Type "swim" to swim across\n').lower()
+    if choice2 == "wait":
+        choice3 = input("You arrive at the island unharmed. There is a house with 3 doors.\n "
+                        "One red, one yellow and one blue. Which colour do you choose?\n").lower()
+        if choice3 == "yellow":
+            print("You found the treasure ! You win")
+        elif choice3 == "blue":
+            print("You've have been eaten by a beast. Game over!")
+        elif choice3 == "red":
+            print("You get burned by a fire. Game over!")
+    elif choice2 == "swim":
+        print("You get attacked by a shark. Game over!")
+    else:
+        print("Game over!")
+
+else:
+    print("Game over!")
